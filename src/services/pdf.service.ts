0b049376e6dfb5e4de19a4465a457f3efaaf8c56
Reznichenko Vladimir <revv@bork.ru>
2024-08-16 20:28:00 +0300

    Добавил no-sandbox в параметры puppeteer чтобы он смог запуститься

--- src/services/pdf.service.ts
+++ src/services/pdf.service.ts
@@ -5,6 +5,7 @@ import puppeteer from 'puppeteer';
 export class PdfService {
   async generatePdf(htmlContent: string): Promise<Uint8Array> {
     const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
       headless: 'shell'
     });
     const page = await browser.newPage();
